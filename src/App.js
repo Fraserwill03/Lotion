efa0614af3f8b4c154264b926fa52aec9408a8ef
Fraserwill03 <williamgfraser2015@gmail.com>
2023-03-07 16:52:23 -0700

    testing with removing Routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Route, useRoutes } from "react-router-dom";
 import Layout from "./components/Layout";
 import View from "./components/View";
 import Editor from "./components/Editor";
 
 
 function App() {
-  // const Base = () => useRoutes([
-  //   { path: '/', element: <Layout /> },
-  //   { path: '/notes', element: <Layout /> },
-  // ]);
+  const Base = () => useRoutes([
+    { path: '/', element: <Layout /> },
+    { path: '/notes', element: <Layout /> },
+  ]);
   
   return (
   <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-        {/* <Base > */}
+        <Base >
           <Route path='/' element={<div className='empty-content'>Select a note, or create a new one.</div>}></Route>
           <Route path='/:index' element={<View/>}></Route>
           <Route path='/:index/edit' element={<Editor />}></Route>
-        {/* </Base> */}
-        </Route>
-      </Routes>
+        </Base>
     </BrowserRouter>
   );
 }
