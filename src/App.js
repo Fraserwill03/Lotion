e9b548f727ff4e041a40e3c1a3d00ac42ef470c3
Fraserwill03 <williamgfraser2015@gmail.com>
2023-03-07 16:26:08 -0700

    changed routing to try to fix netlify error

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useRoutes } from "react-router-dom";
 import Layout from "./components/Layout";
 import View from "./components/View";
 import Editor from "./components/Editor";
 
 
 function App() {
-
+  const Base = () => useRoutes([
+    { path: '/', element: <Layout /> },
+    { path: '/notes', element: <Layout /> },
+  ]);
+  
   return (
-  <BrowserRouter basename='/notes'>
+  <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
+        <Base >
           <Route path='/' element={<div className='empty-content'>Select a note, or create a new one.</div>}></Route>
           <Route path='/:index' element={<View/>}></Route>
           <Route path='/:index/edit' element={<Editor />}></Route>
-        </Route>
+        </Base>
       </Routes>
     </BrowserRouter>
   );
