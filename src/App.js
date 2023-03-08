3dd736295c2b0e2451218e58dbe7223d2efc2e0f
Fraserwill03 <williamgfraser2015@gmail.com>
2023-03-07 17:02:20 -0700

    removed useRoutes, trying multiple route parents in routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import { BrowserRouter, Route, useRoutes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import View from "./components/View";
 import Editor from "./components/Editor";
 
 
 function App() {
-  const Base = () => useRoutes([
-    { path: '/', element: <Layout /> },
-    { path: '/notes', element: <Layout /> },
-  ]);
   
   return (
   <BrowserRouter>
-        <Base >
+      <Routes>
+        <Route path='/' element={<Layout />}>
           <Route path='/' element={<div className='empty-content'>Select a note, or create a new one.</div>}></Route>
           <Route path='/:index' element={<View/>}></Route>
           <Route path='/:index/edit' element={<Editor />}></Route>
-        </Base>
+        </Route>
+        <Route path='/notes' element={<Layout />}>
+          <Route path='/' element={<div className='empty-content'>Select a note, or create a new one.</div>}></Route>
+          <Route path='/:index' element={<View/>}></Route>
+          <Route path='/:index/edit' element={<Editor />}></Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
