694e5f1b1036c4c73d894d28a22a05809109f126
Fraserwill03 <williamgfraser2015@gmail.com>
2023-03-07 18:51:26 -0700

    set default and invalid navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import View from "./components/View";
 import Editor from "./components/Editor";
@@ -10,9 +10,11 @@ function App() {
   <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
+            <Route path='/' element={<Navigate to='/notes' />} /> 
             <Route path='/notes' element={<div className='empty-content'>Select a note, or create a new one.</div>}></Route>
             <Route path='/notes/:index' element={<View/>}></Route>
             <Route path='/notes/:index/edit' element={<Editor />}></Route>
+            <Route path='*' element={<Navigate to='/notes' />} />
           </Route>
         </Routes>
     </BrowserRouter>
